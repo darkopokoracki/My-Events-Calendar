63a2ca259caa78928f0639388e4d709d1920f86c
darkopokoracki <darkopokoracki@gmail.com>
2021-09-22 23:43:59 +0200

    alerts

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const dateInput = document.getElementById('input-date');
 const locationInput = document.getElementById('input-location');
 const tableBody = document.querySelector('.table-body');
 const addButton = document.querySelector('.add-btn');
+const eventForm = document.querySelector('.event-form');
 
 
 function addInTable() {
@@ -20,13 +21,45 @@ function addInTable() {
     locationInput.value = '';
 }
 
+function createAlert(bool) {
+    console.log(bool);
+
+    const alert = document.createElement('div');
+    alert.className = 'alert';
+
+    const closeAlertBtn = document.createElement('span');
+    closeAlertBtn.className = 'close-btn';
+    closeAlertBtn.appendChild(document.createTextNode('X'));
+
+    alert.appendChild(closeAlertBtn);
+
+    if (bool === true) {
+        alert.classList.add('success-alert');
+        alert.appendChild(document.createTextNode(`Succeess: The event is added to the table`));
+    } else {
+        alert.classList.add('error-alert');
+        alert.appendChild(document.createTextNode(`Error: All fields must be filled`));
+    }
+    
+    eventForm.appendChild(alert);
+
+    closeAlertBtn.addEventListener('click', () => {
+        alert.remove();
+    });
+
+    setTimeout(() => {
+        alert.remove();
+    }, 7000);
+}
+
 function formValidation(e) {
     e.preventDefault();
 
     if (eventInput.value === '' || dateInput.value === '' || locationInput.value === '') {
-        console.log('Niste popunili formu');
+        createAlert(false);
     } else {
         addInTable();
+        createAlert(true);
     }
 }
 
