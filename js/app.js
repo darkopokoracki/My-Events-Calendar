b9863c1cab0bd3d1b8d8f125c80808d5933c8d28
darkopokoracki <darkopokoracki@gmail.com>
2021-09-23 16:03:10 +0200

    addInLocalStorage function implemented

--- js/app.js
+++ js/app.js
@@ -5,13 +5,33 @@ const tableBody = document.querySelector('.table-body');
 const addButton = document.querySelector('.add-btn');
 const eventForm = document.querySelector('.event-form');
 
+function updateTable() {
+    
+    for (let i = 0; i < localStorage.length; i++) {
+        const dd = localStorage.getItem(localStorage.key(i));
+        const ff = JSON.parse(dd);
 
-function addInTable() {
+        let tableRow = document.createElement('tr');
+
+        tableRow.innerHTML = `
+            <td>${i + 1}</td>
+            <td>${ff.event}</td>
+            <td>${ff.date}</td>
+            <td>${ff.location}</td>
+        `
+
+        tableBody.appendChild(tableRow); 
+    }      
+}
+
+function addInTable(stringifyEventObject) {
+    const parseEventObject = JSON.parse(stringifyEventObject);
+    let eventsCounter = localStorage.length;
+
     let tableRow = document.createElement('tr');
-    const eventObject = localStorage.getItem('event1');
-    const parseEventObject = JSON.parse(eventObject);
 
     tableRow.innerHTML = `
+        <td>${eventsCounter}</td>
         <td>${parseEventObject.event}</td>
         <td>${parseEventObject.date}</td>
         <td>${parseEventObject.location}</td>
@@ -21,6 +41,7 @@ function addInTable() {
     eventInput.value = '';
     dateInput.value = '';
     locationInput.value = '';
+
 }
 
 function addInLocalStorage(event, date, location) {
@@ -36,12 +57,13 @@ function addInLocalStorage(event, date, location) {
 
     const eventObject = new Event(event, date, location);
 
-
-    localStorage.setItem(`event${eventsCounter + 1}`, JSON.stringify(eventObject));
+    const stringifyEventObject = JSON.stringify(eventObject);
+    localStorage.setItem(`event${eventsCounter + 1}`, stringifyEventObject);
+    
+    addInTable(stringifyEventObject);
 }
 
 function createAlert(bool) {
-    console.log(bool);
 
     const alert = document.createElement('div');
     alert.className = 'alert';
@@ -79,11 +101,10 @@ function formValidation(e) {
     } else {
         createAlert(true);
         addInLocalStorage(eventInput.value, dateInput.value, locationInput.value);
-        addInTable();
     }
 }
 
 
 addButton.addEventListener('click', formValidation);
-
+updateTable();
 // localStorage.clear();
